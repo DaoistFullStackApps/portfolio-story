d6c47efdbb6beb23fce74139e742f30e3e0cd134
daoistcoder <deservekindness@gmail.com>
2023-08-21 23:43:08 +0800

    UPDATA: add divs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,26 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div id="title">
+
+      </div>
+      <div id="background">
+
+      </div>
+      <div id="story">
+
+      </div>
+      <div id="choices">
+
+      </div>
+      <div id="sound">
+        <audio src="" id="bgMusic">
+          <source />
+        </audio>
+        <button id="musicToggle">
+
+        </button>
+      </div>
     </main>
   )
 }

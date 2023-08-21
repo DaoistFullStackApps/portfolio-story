13eb345c362a4f8af5649cdab29669c10854d853
daoistcoder <deservekindness@gmail.com>
2023-08-21 23:45:47 +0800

    ADD: StoryTellerJs

---
+++ hooks/StorryTeller.ts
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StorryTeller() {
+  return (
+    <div></div>
+  )
+}
+
+export default StorryTeller
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import 
 import Image from 'next/image'
 
 export default function Home() {
